4e98f41ff602c53d7be9f913ee5b194534190b33
Hercules Dias Campos <hercules.amphibolite@gmail.com>
2020-04-03 03:15:33 -0400

    progress made with plane spawning, not so much on the distance detection side. Gotta refine methods

---
+++ AR_Cube/Assets/Scripts/InputControls.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARPlaneManager)), RequireComponent(typeof(ARRaycastManager))]
+public class InputControls : MonoBehaviour
+{
+
+    private ARPlaneManager m_ARPlaneManager;
+    private ARRaycastManager m_ARRaycastManager;
+
+    private Transform diamond;
+
+    void Awake() {
+        m_ARPlaneManager = GetComponent<ARPlaneManager>();
+        m_ARRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Not sure whether this if statement is working
+        if (m_ARPlaneManager.trackables.count > 0 && !diamond) {
+            diamond = GameObject.FindGameObjectWithTag("Target").transform;
+        }
+        //This successfully suppresses the creation of new planes
+        if (m_ARPlaneManager.trackables.count >= 1) {
+            m_ARPlaneManager.enabled = false;
+        }
+        //This will beget revisions
+        //I was aware that the usual approach wouldn't work, but it was worth a try
+        if (diamond) {
+            if ((diamond.position - transform.position).sqrMagnitude < 1) {
+                Debug.Log("D:" + diamond.position + ", C:" + transform.position);
+                if (Input.touchCount > 0) {
+                    Touch t = Input.GetTouch(0); 
+                    diamond.transform.Rotate(Vector3.up * t.deltaPosition.x);
+                }
+            }
+        }
+    }
+}
---
+++ AR_Cube/Assets/Scripts/PlaneBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlaneBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject m_PedestalModel;
+    [SerializeField] private GameObject m_DiamondModel;
+
+    //Rationale: other scripts will need to access it
+    public GameObject Pedestal;
+    public GameObject Diamond;
+    void Awake() {
+
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Pedestal = Instantiate(m_PedestalModel, transform.position + Vector3.up * 0.2f, Quaternion.identity, transform) as GameObject;
+        Pedestal.transform.localScale = new Vector3(0.4f, 0.2f, 0.4f);
+        Diamond = Instantiate(m_DiamondModel, transform.position + Vector3.up * 0.55f, Quaternion.Euler(-90f, 0, 0), Pedestal.transform) as GameObject;
+        Diamond.transform.localScale = new Vector3(31.25f, 31.25f, 62.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
